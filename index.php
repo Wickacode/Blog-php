fdef164586f1584a19e736a74c507ef3f93b2f0c
Jessica Garrido <jessica.garrido.di@gmail.com>
2024-03-27 16:14:43 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <a href="accueil.php">Accueil</a>
+                </li>
+                <li>
+                    <a href="articles.php">Articles</a>
+                </li>
+                <li>
+                    <a href="contact.php">Contact</a>
+                </li>
+                <li>
+                    <a href="login.php">Connexion</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
