62f44dc3b673d0451ea6caaff148557ae8dad1bf
Jessica Garrido <jessica.garrido.di@gmail.com>
2024-06-24 22:01:02 +0200

    gestion des erreurs sur la page de connexion et d'inscription (vérification du nb de caractères pour le mdp, mots de passes identiques etc.)

--- src/controllers/AuthController.php
+++ src/controllers/AuthController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Controllers;
+
 use Models\Entity\User;
 use Models\Repository\UsersRepository;
 
@@ -10,7 +11,7 @@ class AuthController extends Controller
     {
         //Gestion de la récupération et de la sauvegarde des données 
         if (isset($_POST['submitRegister'])) {
-            if (!empty($_POST['firstname']) && !empty($_POST['lastname']) && !empty($_POST['pseudo']) &&!empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['confirmPass'])) {
+            if (!empty($_POST['firstname']) && !empty($_POST['lastname']) && !empty($_POST['pseudo']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['confirmPass'])) {
                 $hashedpassword = password_hash($_POST['password'], PASSWORD_BCRYPT);
                 $user = new User([
                     "firstname" => $_POST['firstname'],
@@ -19,25 +20,51 @@ class AuthController extends Controller
                     "pseudo" => $_POST['pseudo'],
                     "password" => $hashedpassword
                 ]);
-                $validAdd= "Votre compte a bien été crée";
+
             } else {
                 $error = "Tous les champs doivent être saisis";
                 echo $this->twig->render('register.html.twig', ["error" => $error]);
             }
-            if($_POST['password'] == $_POST['confirmPass']) {
-                $repository = new UsersRepository();
-                $repository->addUser($user);
+            $passwordLength = strlen($_POST['password']);
+            if ($passwordLength >= 8) {
+                if ($_POST['password'] == $_POST['confirmPass']) {
+                    $repository = new UsersRepository();
+                    $repository->addUser($user);
+                    $validAdd = "Votre compte a bien été crée, vous pouvez maintenant vous connecter";
+                } else {
+                    $error = "Les mots de passe doivent être identiques.";
+                    echo $this->twig->render('register.html.twig', ["error" => $error]);
+                }
+            } else {
+                $error = "Le mot de passe doit contenir minimum 8 caractères.";
+                echo $this->twig->render('register.html.twig', ["error" => $error]);
             }
-        } 
-        // if(isset($validAdd)) {
-        //     echo $this->twig->render('register.html.twig',["validAdd" => $validAdd]);
-        // } else {
-            echo $this->twig->render('register.html.twig');
-        // }    
         }
+        if (isset($validAdd)) {
+            echo $this->twig->render('login.html.twig', ["validAdd" => $validAdd]);
+        } else {
+            echo $this->twig->render('register.html.twig');
+        }
+    }
     public function login()
     {
+        if (isset($_POST['submitLogin'])) {
+            if (!empty($_POST['email']) && !empty($_POST['password'])) {
+                $repository = new UsersRepository();
+                $user = $repository->getUserByEmail($_POST['email']);
+
+                if (password_verify($_POST['password'], $user->getPassword())) {
+                    $_SESSION['auth'] = true;
+                    $_SESSION['userId'] = $user->getId_user();
+                    $_SESSION['role'] = $user->getRole();
+                    echo $this->twig->render('home.html.twig');
+                } else {
+                    $error = "Le mot de passe est incorrect";
+                    echo $this->twig->render('login.html.twig', ["error" => $error]);
+
+                }
+            }
+        }
         echo $this->twig->render('login.html.twig');
     }
-
-} 
+}
