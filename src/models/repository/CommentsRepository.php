cfd13bf91a1994e97b3553901214950f39051bbc
Jessica Garrido <jessica.garrido.di@gmail.com>
2024-07-04 22:29:27 +0200

    gestion des commentaires

--- src/models/repository/CommentsRepository.php
+++ src/models/repository/CommentsRepository.php
@@ -12,8 +12,8 @@ class CommentsRepository
     }
 
     public function addComment(Comment $comment){
-        $sql = 'INSERT INTO comment (content, date_publication, date_modification, statut, delete_comment, id_article, id_user)
-            VALUES (:content, NOW(), NOW(), NULL, 0, :id_article, :id_user)';
+        $sql = 'INSERT INTO comment (content, statut, delete_comment, id_article, id_user)
+            VALUES (:content, NULL, 0, :id_article, :id_user)';
     
             $query = $this->mysqlClient->prepare($sql);
             $query->bindValue('content',$comment->getContent(), PDO::PARAM_STR);
@@ -21,5 +21,16 @@ class CommentsRepository
             $query->bindValue('id_article',$comment->getId_article(), PDO::PARAM_INT);
             $query->execute();
             
-        }
+    }
+
+    public function getComments($id) {
+        $sql = "SELECT * FROM comment AS c,user AS u  WHERE c.id_user = u.id_user AND id_article = $id";
+        $query = $this->mysqlClient->prepare($sql);
+        $query->execute();
+        $dataComments = $query->fetchAll();
+        return $dataComments;
+    }
+    public function approveComment() {
+
+    }
 }
