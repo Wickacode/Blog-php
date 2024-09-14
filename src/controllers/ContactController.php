d8cc90ce43a674797b740ceebeef06cc3d8a5488
Jessica Garrido <jessica.garrido.di@gmail.com>
2024-09-14 23:36:05 +0200

    Inbox management

--- src/controllers/ContactController.php
+++ src/controllers/ContactController.php
@@ -4,8 +4,26 @@ namespace Controllers;
 
 class ContactController extends Controller
 {
-    public function contact():void
+    public function contact(): void
     {
+        if (isset($_POST['submitContact'])) {
+            if (!empty($_POST['email']) && !empty($_POST['message']) && !empty($_POST['objet'])) {
+                $dest = "jessica.garrido.di@gmail.com";
+                $message = $_POST['message'];
+                $email = $_POST['email'];
+                $objet = $_POST['objet'];
+
+                $entetes = "From:" . $email;
+                $entetes .= "Cc:" . $dest;
+                $entetes .= "Content-Type: text/html; charset=iso-8859-1";
+
+                if (mail($dest, $objet, $message, $entetes))
+                    echo "Mail envoyé avec succès.";
+                else
+                    echo "Un problème est survenu.";
+                exit;
+            }
+        }
         echo $this->render('contact.html.twig');
     }
-} 
+}
